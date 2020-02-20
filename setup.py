66623856a8687adb62745adea771cefcf21d7eb0
claire-ceresa <40687281+claire-ceresa@users.noreply.github.com>
2020-02-20 14:30:53 +0100

    Creation du setup.py pour la creation de l'executable

---
+++ setup.py
@@ -0,0 +1,31 @@
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [Executable("main.py", base=base, targetName="Coraliotech.exe")]
+
+packages = ["os", "sqlite3", "Bio", "xlsxwriter"]
+excludes = ['tkinter']
+
+include_files = ["database",
+                 "graphic",
+                 "objects",
+                 "logo.png"]
+
+options =  {
+    "build_exe":
+                {"packages": packages ,
+                "excludes": excludes,
+                 "include_files":include_files,
+                "include_msvcr": True}}
+
+setup(
+    name = "Coraliotech",
+    options = options,
+    version = "1.0",
+    description = 'Cree par Claire Ceresa',
+    executables = executables
+)
