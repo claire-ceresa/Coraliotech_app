21478de7dcbf21d062e08083facd370430e44ca4
claire-ceresa <40687281+claire-ceresa@users.noreply.github.com>
2020-03-24 16:03:18 +0100

    Telechargement des donnees Symbiodiniaceae

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import sys
+import xlsxwriter as x
 from PyQt5.QtWidgets import *
 from graphic.Product_Window import *
 from graphic.Phylogeny_Window import *
@@ -10,6 +11,22 @@ from objects.NCBI_Product import NCBI_Product
 
 Entrez.email = "claire.ceresa@hotmail.fr"
 
-id ="GU722140.1"
-product = NCBI_Product(id=id)
-print(product.cds)
+search = NCBI_Search(request = "Symbiodiniaceae AND mRNA [Title] AND complete [Title] AND cds [Title] ")
+list_ids = search.get_list_ids()
+
+workbook = x.Workbook("host.xlsx")
+worksheet = workbook.add_worksheet("Couples")
+
+for line, id in enumerate(list_ids):
+    print(str(line))
+    product = NCBI_Product(id=id)
+    species = product.species.species
+    feature_source = product.get_feature_by_type("source")
+    if "host" in feature_source.qualifiers:
+        host = feature_source.qualifiers["host"][0]
+    else:
+        host = None
+    row = [species, host]
+    worksheet.write_row(line, 0, row)
+
+workbook.close()
